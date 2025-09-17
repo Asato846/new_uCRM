d7f7501009cd68c44e513b280fada58be9f2597f
Asato846 <hatakeyama.asato01@gmail.com>
2025-09-17 06:08:02 +0000

    app.js & vite.config.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 import './bootstrap';
 import '../css/app.css';
-
+import ApplicationLogo from './Components/ApplicationLogo.vue';
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
